bf46a3906b00ebbdd6b9d45b7dd24126968367f4
amira123bd <bedouiamira123@gmail.com>
2025-12-20 15:41:33 +0100

    EXplored ORM using shell: Object Relational Mapping, the intermediate between python code and DB

--- posts/models.py
+++ posts/models.py
@@ -9,3 +9,8 @@ class Post(models.Model):
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True) 
     #auto_now_add = True means that the date will be set to the current date and time when the object is created.
+
+
+
+    def __str__(self):
+        return self.title
